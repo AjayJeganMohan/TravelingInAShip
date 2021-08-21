7404f95213ad30a7c3a461ea5d1f4681cfbf27b2
AjayJeganMohan <88392341+AjayJeganMohan@users.noreply.github.com>
2021-08-21 10:08:14 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,53 @@
-var weight = [35,38,42,45,43,34,36,41,48,32];
-var sum = 0
-function setup() {
-  createCanvas(400,400);
-  for(var i=0; i<weight.length; i++){
-    
-    sum = sum + weight[i]
-  }
-  var avg = sum/weight.length
-  console.log(avg)
+var path, pathImage
+var jaxxon, jaxxonRuns
+var invisibleWall
+var invisibleWallR
+function preload(){
+  //pre-load images
+    jaxxonRuns = loadAnimation("Runner-1.png", "Runner-2.png")
+    pathImage = loadImage("path.png")
 }
 
-function draw() 
-{
-  background(30);
-}
+function setup(){
+  createCanvas(400,600);
+  //create sprites here
+  jaxxon = createSprite(200,500,10,30)
+  jaxxon.addAnimation("running", jaxxonRuns)
+  jaxxon.scale = 0.04
 
+  path = createSprite(200,200)
+  path.addImage("track", pathImage)
+  path.velocityY = 4
+  path.scale = 1.2
+  path.height = 1007
+  //path.y = 200
+  jaxxon.depth = path.depth + 1 
+  console.log(path.height + ' ' + path.y)
  
+  invisibleWall = createSprite(0,300,50,600)
+  invisibleWall.visible = false
+  
+  invisibleWallR = createSprite(400,300,50,600)
+  invisibleWallR.visible = false
+ 
+}
 
+function draw() {
+background(0)
+ console.log(path.height + ' ' + path.y)
+if(path.y > path.height / 2) {
+  path.y = 200
+}
+
+if(keyDown(RIGHT_ARROW)){
+  jaxxon.x = jaxxon.x+2
+}
+
+if(keyDown(LEFT_ARROW)){
+  jaxxon.x = jaxxon.x-2
+}
+jaxxon.collide(invisibleWallR)
+jaxxon.collide(invisibleWall)
+  drawSprites();
+
+}
